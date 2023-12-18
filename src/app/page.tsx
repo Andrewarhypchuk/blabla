4e17830a0978e02544b9af8b007cffb61a5706a2
Arkhypchuk Andrii <andrewarhypchuk@gmail.com>
2023-12-18 19:36:07 +0200

    added some code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css';
-import { useState } from 'react';
 import HomePage from '@/Pages/home';
 
 const inter = Inter({ subsets: ['latin'] })
