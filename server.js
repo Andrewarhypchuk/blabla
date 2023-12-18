63b62b19cfe71b1eed2d9cce9ffc719cc2dbc05a
Arkhypchuk Andrii <andrewarhypchuk@gmail.com>
2023-12-18 16:05:33 +0200

    dddd

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
 
-  server.get('/', (req, res) => {
-    return app.render(req, res, '/home')
-  })
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
 
+  const PORT = process.env.PORT || 3000;
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Ready on http://localhost:${PORT}`);
+  });
+});
